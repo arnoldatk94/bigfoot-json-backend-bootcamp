89ed4d42f06af1f08a1cc819d7ea0b62a3be2974
arnoldatk94 <arnoldatk94@gmail.com>
2023-03-29 12:19:08 +0800

    working filter

--- controllers/SightingsController.js
+++ controllers/SightingsController.js
@@ -13,9 +13,28 @@ class SightingsController {
     res.json(sightings[req.params.sightingIndex]);
   };
 
+  // listByFilter = async (req, res) => {
+  //   const sightings = await getSightings();
+  //   let filteredSightings = [...sightings];
+  //   const filterParam = req.query;
+  //   console.log(req.query);
+  //   console.log(filterParam);
+  //   // console.log(filteredSightings);
+  //   for (let key in filterParam) {
+  //     // looks thru all the queries
+  //     filteredSightings = filteredSightings.filter(
+  //       // filters the sighting via a loop
+  //       (element) => element[key] == filterParam[key]
+  //     );
+  //   }
+
+  //   res.json(filteredSightings);
+  // };
+
   listByFilter = async (req, res) => {
     const sightings = await getSightings();
     const filterParam = req.params.year;
+    console.log(req.params);
     const key = "YEAR";
     const filteredSightingsList = sightings.filter(
       (item) => item[key] == filterParam
